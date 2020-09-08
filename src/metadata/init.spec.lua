2fc08714cb9842a3cceae4cba52b8b4efa1210e9
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-09-08 11:16:36 -0500

    Update init.spec.lua

--- src/metadata/init.spec.lua
+++ src/metadata/init.spec.lua
@@ -2,6 +2,8 @@ return function()
 	describe("rbxlua-api", function()
 		it("should load", function()
 			local rbxluaApi = require(script.Parent)
+
+			expect(rbxluaApi).to.be.ok()
 		end)
 	end)
 end
