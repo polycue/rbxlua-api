6abed067a9eeecdafc631b174269a5d538e4d040
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-09-08 11:18:29 -0500

    Update fastSpawn.lua

--- src/metadata/fastSpawn.lua
+++ src/metadata/fastSpawn.lua
@@ -4,15 +4,15 @@
 
 return function(func, ...)
 	assert(type(func) == "function")
-	
+
 	local args = {...}
 	local count = select("#", ...)
-	
+
 	local bindable = Instance.new("BindableEvent")
 	bindable.Event:Connect(function()
 		func(unpack(args, 1, count))
 	end)
-	
+
 	bindable:Fire()
 	bindable:Destroy()
 end
